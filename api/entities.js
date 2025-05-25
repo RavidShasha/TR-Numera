dc99568f06b5342a11584aacf20d3d7a9403d456
RavidShasha <ravid.s@hotmail.com>
2025-05-25 10:34:15 +0300

    Add files via upload

---
+++ api/entities.js
@@ -0,0 +1,27 @@
+import { base44 } from './base44Client';
+
+
+export const Project = base44.entities.Project;
+
+export const Proposal = base44.entities.Proposal;
+
+export const ServiceCategory = base44.entities.ServiceCategory;
+
+export const Review = base44.entities.Review;
+
+export const Message = base44.entities.Message;
+
+export const ProfileUpdateRequest = base44.entities.ProfileUpdateRequest;
+
+export const ProjectFile = base44.entities.ProjectFile;
+
+export const Notification = base44.entities.Notification;
+
+export const FavoriteProject = base44.entities.FavoriteProject;
+
+export const PaymentLog = base44.entities.PaymentLog;
+
+
+
+// auth sdk:
+export const User = base44.auth;
